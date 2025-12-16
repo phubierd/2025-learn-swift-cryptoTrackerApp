fbd72940cd2fdb6c84dcca5ccfe25f9bbeef4fb3
phuct <phubierd@gmail.com>
2025-12-16 23:32:24 +0700

    download image to cache folder & check exist image before download

--- SwiftfulCrypto/Services/CoinImageService.swift
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -14,21 +14,40 @@ class CoinImageService {
     @Published var image:UIImage? = nil
     private var imageSubscription:AnyCancellable?
     private let coin:CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName:String
     
     init (coin:CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage(){
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName){
+            image = savedImage
+            print("Retrieved image from File Manager")
+        }else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage(){
+        print("Downloading image now !!")
         guard let url = URL(string: coin.image)else {return}
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 return UIImage(data:data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompetion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self,
+                    let downloadedImage = returnedImage
+                else {return}
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
